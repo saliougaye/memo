73b9ca61953d08f2e995313d3ac0f8c71c613513
Saliou <gayesaliou29@gmail.com>
2022-08-31 14:58:22 +0200

    chore: add create memo procedure

--- src/common/validation/memo.ts
+++ src/common/validation/memo.ts
@@ -1,5 +1,15 @@
 import * as z from 'zod';
 
 export const addMemoSchema = z.object({
-    
+    title: z.string(),
+    description: z.string().max(30).nullish(),
+    reminder: z.date().min(new Date()),
+    notify: z.boolean().default(true),
+    category: z.object({
+        id: z.string().optional(), // uuid check
+        name: z.string(),
+        color: z.string(),
+    }).optional()
 });
+
+export type IAddMemoSchema = z.infer<typeof addMemoSchema>;
--- src/server/routers/memo.ts
+++ src/server/routers/memo.ts
@@ -1,4 +1,5 @@
 import * as trpc from '@trpc/server';
+import { addMemoSchema } from '../../common/validation/memo';
 import { createRouter } from "../create-router";
 
 
@@ -15,11 +16,31 @@ export const memoRouter = createRouter()
         }
     })
     .mutation('add', {
+        input: addMemoSchema,
         resolve: async ({ input, ctx }) => {
-            throw new trpc.TRPCError({
-                code: "BAD_REQUEST",
-                message: "Method not implemented"
+            const { title, description, reminder, notify, category } = input;
+
+            const memoCreated = await ctx.prisma.memo.create({
+                data: {
+                    title: title,
+                    description: description ?? '',
+                    reminder: reminder,
+                    notify: notify,
+                    category: !category ? undefined : {
+                        connectOrCreate: {
+                            where: {
+                                id: category.id
+                            },
+                            create: {
+                                name: category.name,
+                                color: category.color,
+                            }
+                        }
+                    }
+                }
             })
+            
+            return memoCreated;
         }
     })
     .mutation('edit', {
